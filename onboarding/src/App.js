4dea8c2ff38dc51673eb68e0a4e3e9d32e455a8f
jaredhall18 <jaredhall18@gmail.com>
2021-09-15 12:41:21 -0600

    created filestructure for React app. linked Form.js to app

---
+++ onboarding/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+import Form from './components/Form'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header><h1>Form</h1></header>
+    <Form />
+    </div>
+  );
+}
+
+export default App;
